0791f823ad874fdb07436e3af20bc422b5aa7ed7
Christopher Orr <chris@orr.me.uk>
2015-04-02 11:21:28 -0700

    Fix NPE upon loading config page that doesn't yet have a DeployDB trigger.
    Fixes #18.

--- src/main/resources/org/jenkinsci/plugins/deploydb/DeployDbTrigger/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/deploydb/DeployDbTrigger/config.groovy
@@ -3,6 +3,6 @@ package org.jenkinsci.plugins.deploydb.DeployDbTrigger;
 f = namespace('/lib/form')
 
 f.entry(title: _("Event types")) {
-    f.hetero_list(descriptors: descriptor.eventDescriptors, items: instance.triggerEventTypes,
+    f.hetero_list(descriptors: descriptor.eventDescriptors, items: instance?.triggerEventTypes,
                   name: 'triggerEventTypes', hasHeader: true)
 }
